5ce35bd88ec92bf563a00bcff078a86e94ac383f
bakemono-san <mousdef34@gmail.com>
2024-08-03 17:38:17 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import StatusContainer from "@/components/StatusContainer";
 import { Post } from "../../utils/type";
 
 export default async function Home() {
+  if(!process.env.NEXT_PUBLIC_API_URL){
+    return null;
+  }
   const posts = await getData();
   return (
     <main className="flex flex-col w-full gap-16 py-12 px-52 ml-[250px]">
@@ -15,9 +18,6 @@ export default async function Home() {
 }
 
 async function getData(){
-  if(!process.env.NEXT_PUBLIC_API_URL){
-    return null;
-  }
   try {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`)
   if (!res.ok) {
